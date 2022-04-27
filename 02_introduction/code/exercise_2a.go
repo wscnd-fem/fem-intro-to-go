591eade5f186ccc75389e14b5d599ae53606c15c
wscnd <72019998+wscnd@users.noreply.github.com>
2022-04-27 12:16:41 -0300

    02_introduction: printing exercise 2a

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Printf(`"Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t."`, "wscnd", "Brazil", 3, true)
+}
