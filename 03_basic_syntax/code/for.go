eae101850ecbda66f5a88213d563b5f4fbb43dfe
wscnd <72019998+wscnd@users.noreply.github.com>
2022-04-28 10:11:22 -0300

    03_basic_syntax: for loops

--- 03_basic_syntax/code/for.go
+++ 03_basic_syntax/code/for.go
@@ -2,33 +2,31 @@
 
 package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
+func main() {
 
-// 	// 	// ****************************
+	// 	// ****************************
 
-// 	// i := 1
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
 
-// 	for i := 1; i <= 100; i++ {
-// 		fmt.Println(i)
-// 	}
+	// 	// ****************************
 
-// 	// 	// ****************************
+	i := 1
 
-// 	// i := 1
+	for i <= 100 {
+		fmt.Println(i)
+		// This will behave like a while loop
+		i += 1
+	}
 
-// 	// for i <= 100 {
-// 	// 	fmt.Println(i)
-// 	// 	// This will behave like a while loop
-// 	// 	i += 1
-// 	// }
+	// 	// ****************************
 
-// 	// 	// ****************************
+	var mySentence = "This is a sentence"
 
-// 	// var mySentence = "This is a sentence"
-
-// 	// for index, letter := range mySentence {
-// 	// 	fmt.Println("Index:", index, "Letter:", letter)
-// 	// }
-// }
+	for index, letter := range mySentence {
+		fmt.Println("Index:", index, "Letter (int32):", letter, "Letter (string):", string(letter))
+	}
+}
