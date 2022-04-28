16ece3f5df8f60ff1b02f1cd548025d4ea63722d
wscnd <72019998+wscnd@users.noreply.github.com>
2022-04-28 14:54:54 -0300

    04_complex_structures: functions and variadic functions

--- 04_complex_structures/code/functions.go
+++ 04_complex_structures/code/functions.go
@@ -1,15 +1,35 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
-// 	ageOfSally = age1
-// 	ageOfBob = age2
-// 	return
-// }
+func namedReturnPrintAge(age int) (ageOfW int, ageOfR int) {
+	ageOfR = age + 21
+	ageOfW = age + 20
+	return
+}
+
+func spreadPrintAge(f func(a ...any) (int, error), ages ...int) {
+	for _, value := range ages {
+		f(value + 20)
+	}
+}
+
+func fn[T any](f func(a T) T, b T) (T, error) {
+	return f(b), nil
+}
+
+func funcao(a int) int {
+	return a * 10
+}
 
 func main() {
-	// 	x, y := printAge(10, 21)
-	// 	fmt.Println(x)
-	// 	fmt.Println(y)
+	fmt.Println(namedReturnPrintAge(0))
+	ageA, ageB := namedReturnPrintAge(0)
+
+	fmt.Println("ageA:", ageA, "ageB:", ageB)
+	spreadPrintAge(fmt.Println, 1, 2)
+
+	x, y := fn(funcao, 2)
+
+	fmt.Println("x:", x, "y:", y)
 }
